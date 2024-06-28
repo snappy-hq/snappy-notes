84006a762f186e3dd5d988f30247b554efe61117
Piush Bose <piushbasu@gmail.com>
2024-06-28 21:23:04 +0530

    code: learning more about the tiptap js

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,6 @@ import Tiptap from "~/components/tiptap";
 export default function Home() {
   return (
     <main>
-      <h1>Hello this is Snappy Notes</h1>
       <Tiptap />
     </main>
   );
--- src/components/tiptap/index.tsx
+++ src/components/tiptap/index.tsx
@@ -1,19 +1,39 @@
-'use client'
+"use client";
 
-import { useEditor, EditorContent } from '@tiptap/react'
-import StarterKit from '@tiptap/starter-kit'
+import { useEditor, EditorContent, generateHTML } from "@tiptap/react";
+import StarterKit from "@tiptap/starter-kit";
+import React from "react";
+import { Button } from "../ui/button";
 
 const Tiptap = () => {
   const editor = useEditor({
-    extensions: [
-      StarterKit,
-    ],
-    content: '<p>Hello World! 🌎️</p>',
-  })
+    extensions: [StarterKit],
+    content: "<p>Hello World! 🌎️</p>",
+    injectCSS: false,
+  });
 
   return (
-    <EditorContent editor={editor} controls={true} />
-  )
-}
+    <React.Fragment>
+      <Button onClick={() => editor?.chain().focus().toggleBold().run()}>
+        Bold
+      </Button>
+      <Button onClick={() => editor?.chain().focus().toggleItalic().run()}>
+        Italic
+      </Button>
+      <Button onClick={() => editor?.chain().focus().undo().run()}>Undo</Button>
+      <Button onClick={() => editor?.chain().focus().redo().run()}>Redo</Button>
+      <Button
+        onClick={() => {
+          console.log(editor?.getHTML());
+          console.log(generateHTML(editor?.getJSON()!, [StarterKit]));
+        }}
+      >
+        Print the html
+      </Button>
+      <EditorContent editor={editor} controls={true} />
+      {/* <textarea id="editor-input" /> */}
+    </React.Fragment>
+  );
+};
 
-export default Tiptap
+export default Tiptap;
